9f3682b50158b5ccd09e49ed79b866eb4c713059
hot_beans <106505475+hot-beans24@users.noreply.github.com>
2023-02-01 09:12:20 +0900

    introduction emotion and chakraUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <title>Portfolio</title>
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  )
 }
+
+export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from '@chakra-ui/react'
 
-export default function Document() {
+const Document = () => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
+        <ColorModeScript />
         <Main />
         <NextScript />
       </body>
     </Html>
   )
 }
+
+export default Document
